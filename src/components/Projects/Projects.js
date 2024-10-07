fc227f27dbf65b64301b0c013c0b6a58263bd515
Massimiliano Bruchi <massimiliano@3lue9.dev>
2024-10-07 21:37:23 +0200

    lil fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -24,23 +24,18 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={bitsOfCode}
               isBlog={false}
-              title="TakeBY"
-              description="TakeBy è una piattaforma online che crea una rete di negozi, consentendo agli utenti di prenotare prodotti e servizi da diverse attività, come farmacie, supermercati, negozi di abbigliamento, saloni di bellezza e altro ancora, attraverso un'unica app."
-              ghLink="https://github.com/TakeBy-it"
-              demoLink="https://www.takeby.it/"
+              title="audio service & live music"
+              description="da oramai un anno collaboro con una società teatrale che mi ha permesso di accresscere la mia passione verso la musica e il lato più sconociuto di questa, e grazie a questo sono riuscito a far partire dei progetti come portare la musica nelle scuole e aiutare piccole band per farle crescere"
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Regia teatrale"
-              description="grazie a delle connessioni, me è stato offerto un posto in una compagnia teatrale, che mi ha permesso di imparare e espandere la mia passione per la musica e l'audio live"
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="il giornale di Ugo e Radio Ugo"
+              description="Radio Ugo è una web radio nata nel 2021 all'interno dell'ITI Severi di Padova, che trasmette alcune volte all'anno all'interno dell'istituto"
             />
           </Col>
         </Row>
